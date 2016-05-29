5c97ade1448e2aa19f9d4136d58dc318bb68d5f0
Bryan Mytko <bryanmytko@gmail.com>
2016-05-29 17:43:38 -0400

    Add main D3 method with api key and locale

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 var unirest = require('unirest');
 var merge = require('merge');
 var endpoints = require('./endpoints');
+
+function D3(api_key, locale){
+  this.api_key = api_key;
+  this.locale = locale;
+
+  return this;
+}
