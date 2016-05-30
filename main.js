ecca5e854746556563dd7a1d31d57f5340b92d1b
Bryan Mytko <bryanmytko@gmail.com>
2016-05-30 17:24:43 -0400

    Change main.js to export contructor function

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var unirest = require('unirest');
 var merge = require('merge');
-var endpoints = require('./endpoints');
 
-function D3(api_key, locale){
-  this.api_key = api_key;
-  this.locale = locale;
+var CustomError = require('./custom-error');
+var Endpoints = require('./endpoints');
+
+module.exports = function D3(api_key, locale){
+  this.api_key = api_key; //|| throw new CustomError('No API key set!', 1);
+  this.locale = locale || "enUS";
 
   return this;
 }
