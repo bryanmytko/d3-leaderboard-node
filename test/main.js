cd98afdb004b558aee0fcc5d497f2ce6dc9ccda6
Bryan Mytko <bryanmytko@gmail.com>
2016-06-01 18:15:02 -0400

    Add tests for locale

--- test/main.js
+++ test/main.js
@@ -1,26 +1,40 @@
-var chai = require("chai"),
+var chai = require('chai'),
   expect = chai.expect,
-  util   = require("util"),
-  sinon  = require("sinon"),
-  sinonChai = require("sinon-chai");
+  util   = require('util'),
+  sinon  = require('sinon'),
+  sinonChai = require('sinon-chai');
 
 var envVars = require('./env-vars'),
   api_key = envVars['BLIZZARD_API_KEY'];
 
 chai.use(sinonChai);
 
-var D3 = require("../main.js"),
-  client = new D3(api_key, "enUS");
+var D3 = require('../main.js'),
+  client = new D3(api_key, 'enUS');
 
-describe("D3", function(){
-  describe("when instantiated", function(){
-    it("returns a client", function(){
+describe('D3', function(){
+  describe('when instantiated', function(){
+    it('returns a client', function(){
       expect(client).to.be.an.instanceof(D3);
     });
   });
 
-  describe("when instantiated without an API key", function(){
-    it("throw API key error", function(){
+  describe('when instantiated with a locale', function(){
+    it('has a locale', function(){
+      var client = new D3(api_key, 'jaJP');
+      expect(client.locale).to.be.equal('jaJP');
+    });
+  });
+
+  describe('when instantiated without a locale', function(){
+    it('has a default locale', function(){
+      var client = new D3(api_key);
+      expect(client.locale).to.not.be.undefined;
+    });
+  });
+
+  describe('when instantiated without an API key', function(){
+    it('throw API key error', function(){
       var fn = function(){ new D3(); }
       expect(fn).to.throw('CustomError', 'No API key set!');
     });
