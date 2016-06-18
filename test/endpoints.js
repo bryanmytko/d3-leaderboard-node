b6c804425d07d017e1806991b158ce47ba1ddc8e
Bryan Mytko <bryanmytko@gmail.com>
2016-06-18 03:13:42 -0400

    Add test for eras endpoint

--- test/endpoints.js
+++ test/endpoints.js
@@ -12,7 +12,7 @@ var D3 = require('../main.js'),
 
 describe('D3 Endpoints', function(){
   describe('/seasons/', function(){
-    it('returns data', function(done){
+    it('returns seasons data', function(done){
       client.seasons(function(error, response){
         expect(error).to.be.null;
         expect(response).to.not.be.undefined;
@@ -22,8 +22,18 @@ describe('D3 Endpoints', function(){
   });
 
   describe('/seasons/:id', function(){
-    it('returns data', function(done){
+    it('returns season data', function(done){
       client.seasons({ id: 5 }, function(error, response){
+        expect(error).to.be.null;
+        expect(response).to.not.be.undefined;
+        done();
+      });
+    });
+  });
+
+  describe('/era', function(){
+    it('returns era data', function(done){
+      client.eras(function(error, response){
         expect(error).to.be.null;
         expect(response).to.not.be.undefined;
         done();
